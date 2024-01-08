959ee296f4fbf2f85d2950f9838cac789809f3e8
yanqi <3224848830@qq.com>
2024-01-09 02:26:26 +0800

    fix: git问题

---
+++ my-project-frontend/src/views/WelcomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div style="width: 100vw; height:100vh; overflow: hidden; display: flex">
+    <div style="flex: 1;">
+      <el-image style="width: 100%; height: 100%" fit="cover"
+        src="https://npm.elemecdn.com/yanqi1711-picx/20220423/112.jinldrg0fg0.webp" />
+
+    </div>
+    <div class="welcome-title">
+      <div style="font-size:30px; font-weight: bold;">1111111111111111</div>
+      <div style="margin-top: 10px">2222222222222222222222222222222</div>
+      <div style="margin-top: 5px">33333333333333333333333</div>
+    </div>
+    <div class="right-card">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.welcome-title {
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
+  color: white;
+  text-shadow: 0 0 10px black;
+}
+.right-card {
+  width: 400px;
+  z-index: 1;
+  background-color: white;
+}
+</style>
