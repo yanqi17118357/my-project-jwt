53a00d24e4b2abb556da313f9dd0e766452ff845
yanqi <3224848830@qq.com>
2024-01-11 23:33:43 +0800

    fix: ElMessage没有样式

--- my-project-frontend/src/main.js
+++ my-project-frontend/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import 'element-plus/theme-chalk/el-message.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 
 axios.defaults.baseURL = 'http://localhost:8080'
 
